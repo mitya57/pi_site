15c260d066ea9ab5998a04f3da2e6127b83bd10b
Dmitry Shachnev <mitya57@gmail.com>
2025-12-24 20:59:13 +0300

    yaml_to_elibrary.py: Make the file closer to what xmlrisc.ru produces

--- yaml_to_elibrary.py
+++ yaml_to_elibrary.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 import sys
 import os
 import re
+from datetime import date
 
 
 def generate_elibrary_xml(yaml_file, output_file):
@@ -16,13 +17,14 @@ def generate_elibrary_xml(yaml_file, output_file):
     volume, month, year = name_part.split("_")
 
     journal = ET.Element("journal")
-    ET.SubElement(journal, "titleid").text = "32250"
     ET.SubElement(journal, "issn").text = "2220-3397"
 
-    journal_info = ET.SubElement(journal, "journalInfo", lang="RUS")
+    journal_info = ET.SubElement(journal, "journalInfo", lang="ru")
     ET.SubElement(journal_info, "title").text = "Программная инженерия"
+    ET.SubElement(journal_info, "Title").text = "Software Engineering"
+    ET.SubElement(journal_info, "publ").text = "Новые технологии"
 
-    issue = ET.SubElement(journal, "issue", type="ISS")
+    issue = ET.SubElement(journal, "issue")
     ET.SubElement(issue, "volume").text = volume
     ET.SubElement(issue, "number").text = month
     ET.SubElement(issue, "dateUni").text = year
@@ -31,26 +33,21 @@ def generate_elibrary_xml(yaml_file, output_file):
     _, last_page = data["content"][-1]["item_pages"].split("—")
     ET.SubElement(issue, "pages").text = f"{first_page}-{last_page}"
 
-    codes = ET.SubElement(issue, "codes")
-    ET.SubElement(codes, "doi").text = f"10.17587/prin.{volume}"
-
     articles_elem = ET.SubElement(issue, "articles")
 
-    for idx, article in enumerate(data["content"], 1):
+    for article in data["content"]:
         article_elem = ET.SubElement(articles_elem, "article")
-        ET.SubElement(article_elem, "langPubl").text = "RUS"
         ET.SubElement(article_elem, "artType").text = "RAR"  # Research Article
-        ET.SubElement(article_elem, "pages").text = article["item_pages"]
+        ET.SubElement(article_elem, "pages").text = article["item_pages"].replace("—", "-")
 
         codes_elem = ET.SubElement(article_elem, "codes")
         ET.SubElement(codes_elem, "doi").text = f"10.17587/prin.{article['doi']}"
         ET.SubElement(codes_elem, "edn").text = article["edn"]
-        ET.SubElement(codes_elem, "udk").text = article["udk"]
 
         authors_elem = ET.SubElement(article_elem, "authors")
-        for auth_idx, author in enumerate(article["item_authors_v2"], 1):
-            author_elem = ET.SubElement(authors_elem, "author", num=str(auth_idx))
-            individ = ET.SubElement(author_elem, "individInfo", lang="RUS")
+        for author in article["item_authors_v2"]:
+            author_elem = ET.SubElement(authors_elem, "author")
+            individ = ET.SubElement(author_elem, "individInfo", lang="ru")
             ET.SubElement(individ, "surname").text = author["last_name"]
             ET.SubElement(individ, "initials").text = author["initials"]
 
@@ -60,22 +57,22 @@ def generate_elibrary_xml(yaml_file, output_file):
             ET.SubElement(individ, "email").text = author["email"]
 
         art_titles = ET.SubElement(article_elem, "artTitles")
-        ET.SubElement(art_titles, "artTitle", lang="RUS").text = article["item_name"]
+        ET.SubElement(art_titles, "artTitle", lang="ru").text = article["item_name"]
 
         abstracts_elem = ET.SubElement(article_elem, "abstracts")
-        abstract_elem = ET.SubElement(abstracts_elem, "abstract", lang="RUS")
+        abstract_elem = ET.SubElement(abstracts_elem, "abstract", lang="ru")
         clean_text = re.sub("<[^>]+>", "", article["item_annot"])
         abstract_elem.text = clean_text.strip()
 
         keywords_elem = ET.SubElement(article_elem, "keywords")
-        kwd_group = ET.SubElement(keywords_elem, "kwdGroup", lang="RUS")
+        kwd_group = ET.SubElement(keywords_elem, "kwdGroup", lang="ru")
         for kwd in article["item_keywords"].split(","):
             ET.SubElement(kwd_group, "keyword").text = kwd.strip()
 
         dates_elem = ET.SubElement(article_elem, "dates")
         ET.SubElement(dates_elem, "dateReceived").text = article["item_rec_on"].isoformat()
         ET.SubElement(dates_elem, "dateAccepted").text = article["item_acc_on"].isoformat()
-        ET.SubElement(dates_elem, "datePublication").text = year
+        ET.SubElement(dates_elem, "datePublication").text = date(int(year), int(month), 1).isoformat()
 
     tree = ET.ElementTree(journal)
     ET.indent(tree)
